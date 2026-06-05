02b3781380d05beaec54e7948740cc173d562c2a
Ungravity <ungravity@MacBook-Pro-Ungravity.local>
2026-06-05 23:27:42 +0300

    updates

--- init.lua
+++ init.lua
@@ -1,16 +1,30 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.termguicolors = true
+
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
 
--- init.lua (или theme.lua)
-local ok, _ = pcall(vim.cmd, "colorscheme tokyonight")
-if not ok then
-  vim.notify("Theme 'catppuccin' not found; using default.", vim.log.levels.WARN)
+local function apply_tokyonight_cursor_hl()
+  vim.api.nvim_set_hl(0, "Cursor", { bg = "#faa2f7", fg = "NONE" })
+  vim.api.nvim_set_hl(0, "CursorLine", { bg = "#171825" })
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#b9e981", bold = true })
 end
-vim.opt.termguicolors = true
+
+local ok = pcall(vim.cmd, "colorscheme tokyonight")
+if not ok then
+  vim.notify("Theme 'tokyonight' not found; using default.", vim.log.levels.WARN)
+else
+  apply_tokyonight_cursor_hl()
+end
+
 vim.opt.cursorline = true
-vim.cmd([[highlight CursorLine guibg=#2a2a2a]])
 vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "tokyonight",
+  callback = apply_tokyonight_cursor_hl,
+})
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,7 @@ keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to left window" })
 keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to right window" })
 keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to lower window" })
 keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to upper window" })
+
+vim.keymap.set("n", "<C-n>", ":botright vnew<CR>")
+vim.keymap.set("n", "<Tab>", ":wincmd w<CR>")
+vim.keymap.set("n", "<S-Tab>", ":wincmd r<CR>")
